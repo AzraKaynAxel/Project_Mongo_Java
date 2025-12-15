cc25b3f9d3d46ea55cd03be53b84b6d1b6eb57cc
AzraKaynAxel <154325193+AzraKaynAxel@users.noreply.github.com>
2025-12-15 20:55:03 +0100

    Version withe read me (#10)
    * ConfigBDD
    
    * J'ajoute plusieurs élément dans ma DB
    
    * Modification d'une donnee de ma BDD
    
    * Supprimer une proriété d'un produit existant
    
    * Manipulation de tableaux
    
    * Suppression d'élément
    
    * Recherche des élément
    
    * Version Finale avec le ReadME compléter
    
    * V Final
    
    ---------
    
    Co-authored-by: AzraKayn/Axel <axforjob.hay@gmail>

--- src/main/java/RunnerApplication.java
+++ src/main/java/RunnerApplication.java
@@ -57,7 +57,7 @@ public class RunnerApplication {
             );
 
             Map<String, Object> createdMany = mongoManager.createManyDocuments(documents);
-            System.out.println("createdMany: " + createdMany + " à bien été créer avec succès");*/
+            System.out.println("createdMany: " + createdMany + " à bien été créer avec succès");
 
         // Mise à jour d'une ou plusieurs données
             // Mes à jour un champ du document
@@ -68,18 +68,18 @@ public class RunnerApplication {
             System.out.println("update_many_documents: " + updateOne);
 
             // Rajoute un champ au document
-            Map<String, Object> updateOne = mongoManager.updateOneDocument(
+            Map<String, Object> updateOneField = mongoManager.updateOneDocument(
                     new Document(documentWatermelon),
                     new Document("$set", new Document("description", "This fruits have been import of Hawaii"))
             );
-            System.out.println("update_one_document: " + updateOne);
+            System.out.println("update_one_document: " + updateOneField);
 
             // Supprimer une propriété d'un produit existant
-            Map<String, Object> updatedOne = mongoManager.updateOneDocument(
+            Map<String, Object> updatedOneProperties = mongoManager.updateOneDocument(
                     new Document(documentWatermelon),
                     new Document("$unset", new Document("description", ""))
             );
-            System.out.println("update_one_document: " + updatedOne);
+            System.out.println("update_one_document: " + updatedOneProperties);
 
         // Manipulation de tableau
             // Ajouter un élément à un tableau
